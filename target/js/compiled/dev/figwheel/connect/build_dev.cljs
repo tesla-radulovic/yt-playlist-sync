11168d53777926ac38740a8b8cefcb840aaf713a
Tesla <tesla.radulovic@proton.me>
2022-12-03 04:18:11 -0500

    gitignore2

--- target/js/compiled/dev/figwheel/connect/build_dev.cljs
+++
@@ -1,3 +0,0 @@
-(ns figwheel.connect.build-dev (:require [figwheel.client] [yt-playlist-sync.core] [figwheel.client.utils]))
-(figwheel.client/start {:build-id "dev", :websocket-url "ws://localhost:3449/figwheel-ws"})
-
